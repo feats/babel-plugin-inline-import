b5061c57482e7700097b399a6305a30e7279f1b1
Eric Dobbertin <aldeed@gmail.com>
2018-05-31 19:07:06 -0500

    fix: remove Meteor code that is no longer needed

--- plugin/index.js
+++ plugin/index.js
@@ -16,11 +16,6 @@ export default function({ types: t }) {
                   throw new Error(`Destructuring inlined import is not allowed. Check the import statement for '${givenPath}'`);
                 }
 
-                // Here we detect the use of Meteor by checking global.meteorBabelHelpers
-                if(global.meteorBabelHelpers && BabelInlineImportHelper.hasRoot(reference)) {
-                  reference = BabelInlineImportHelper.transformRelativeToRootPath(reference);
-                }
-
                 const id = path.node.specifiers[0].local.name;
                 const content = BabelInlineImportHelper.getContents(givenPath, reference);
                 const variable = t.variableDeclarator(t.identifier(id), t.stringLiteral(content));

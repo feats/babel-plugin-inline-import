b2f357c99e673b26701274179ea722ec1e703372
Benedikt Rötsch <info@benedikt-roetsch.de>
2019-04-18 14:41:05 +0700

    upgrade dependencies to be ready for babel 7

--- test/plugin.spec.js
+++ test/plugin.spec.js
@@ -1,38 +1,56 @@
 import BabelInlineImportPlugin from '../plugin';
-import * as babel from 'babel-core';
+import * as babel from '@babel/core';
 
 describe('Babel Inline Import - Plugin', () => {
   describe('Babel Plugin', () => {
     it('transforms the import statement into a variable with the intended content', () => {
-      const transformedCode = babel.transform("import SomeExample from './fixtures/example.raw';", {
-        filename: __filename,
-        plugins: [BabelInlineImportPlugin]
-      });
+      const transformedCode = babel.transform(
+        "import SomeExample from './fixtures/example.raw';",
+        {
+          filename: __filename,
+          plugins: [BabelInlineImportPlugin]
+        }
+      );
 
-      expect(transformedCode.code).to.equal(`/* babel-plugin-inline-import './fixtures/example.raw' */var SomeExample = 'a raw content\\n';`);
+      expect(transformedCode.code).to.equal(`"use strict";
+
+/* babel-plugin-inline-import './fixtures/example.raw' */
+const SomeExample = "a raw content\\n";`);
     });
 
     it('accepts different extensions', () => {
-      const transformedCode = babel.transform("import SomeExample from './fixtures/example.py';", {
-        filename: __filename,
-        plugins: [[
-          BabelInlineImportPlugin, {
-            extensions: [
-              '.py'
+      const transformedCode = babel.transform(
+        "import SomeExample from './fixtures/example.py';",
+        {
+          filename: __filename,
+          plugins: [
+            [
+              BabelInlineImportPlugin,
+              {
+                extensions: ['.py']
+              }
             ]
-          }
-        ]]
-      });
+          ]
+        }
+      );
 
-      expect(transformedCode.code).to.equal(`/* babel-plugin-inline-import './fixtures/example.py' */var SomeExample = 'print 1 + 1\\n';`);
+      expect(transformedCode.code).to.equal(
+        `"use strict";
+
+/* babel-plugin-inline-import './fixtures/example.py' */
+const SomeExample = "print 1 + 1\\n";`
+      );
     });
 
     it('throws error when importing with destructuring', () => {
       expect(() => {
-        babel.transform("import { SomeExample, AnotherExample } from './fixtures/example.raw';", {
-          filename: __filename,
-          plugins: [BabelInlineImportPlugin]
-        });
+        babel.transform(
+          "import { SomeExample, AnotherExample } from './fixtures/example.raw';",
+          {
+            filename: __filename,
+            plugins: [BabelInlineImportPlugin]
+          }
+        );
       }).to.throw(Error);
     });
   });
